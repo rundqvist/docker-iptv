effa9bb46113289310dd268a875f0daed5b92aa9
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-09-13 11:24:26 +0200

    Update update.sh

--- root/app/iptv/update.sh
+++ root/app/iptv/update.sh
@@ -7,63 +7,106 @@ fi
 
 CACHE_PATH="/cache/iptv/"
 EPG_CACHE_PATH=$CACHE_PATH"epg/"
+EPG_OUTPUT_PATH=$CACHE_PATH
 mkdir -p $EPG_CACHE_PATH
+mkdir -p $EPG_OUTPUT_PATH
 
 while [ $DAEMON -eq 1 ] ; do
 
     #
     # Download epg
     #
-    for service in $(var IPTV_SERVICES) ; do
+    if [ "$(var epgUpdatedDate)" != "$(date +%Y-%m-%d)" ] ; then
 
-        provider="$(var -k provider $service)"
-        format="$(var -k format $provider)"
-        log -i iptv "Provider: $provider"
-        log -i iptv "Format: $format"
-        
-        count=0
-        while [ $count -ne $(var IPTV_DAYS) ] ; do
+        log -i iptv "Updating EPG"
+        log -d iptv "Last update was: $(var epgUpdatedDate)"
 
-            filedate=$(date +%Y-%m-%d -d "+$count day")
+        for service in $(var IPTV_SERVICES) ; do
+
+            provider="$(var -k provider $service)"
+            format="$(var -k format $provider)"
+            log -v iptv "Service: $service, provider: $provider, format: $format"
             
-            for channel in $(var -k channels $service) ; do
-                url=$(echo "$format" | sed -e "s/{provider}/$provider/" -e "s/{channel}/$channel/" -e "s/{date}/$filedate/")
-                file=${url##*/}
+            count=0
+            while [ $count -ne $(var IPTV_DAYS) ] ; do
 
-                if [ ! -f $EPG_CACHE_PATH$file ] ; then
-                    log -i iptv "download: $url to $EPG_CACHE_PATH$file"
+                filedate=$(date +%Y-%m-%d -d "+$count day")
+                
+                for channel in $(var -k channels $service) ; do
+                    url=$(echo "$format" | sed -e "s/{provider}/$provider/" -e "s/{channel}/$channel/" -e "s/{date}/$filedate/")
+                    file=${url##*/}
 
-                    wget -P $EPG_CACHE_PATH $url
-                    #download: http://xmltv.xmltv.se/svt1.svt.se_2020-09-13.xml to /cache/iptv/epg/svt1.svt.se_2020-09-13.xml
-                else
-                    log -i iptv "file $EPG_CACHE_PATH$file exists"
-                fi
+                    if [ ! -f $EPG_CACHE_PATH$file ] ; then
 
+                        log -v iptv "Download: $url to $EPG_CACHE_PATH$file"
+                        wget -P $EPG_CACHE_PATH $url
+
+                        if [ $? -eq 0 ] ; then
+                            var epgUpdated true
+                            var epgUpdatedDate $(date +%Y-%m-%d)
+                        else
+                            log -e iptv "Failed to download $url"
+                        fi
+                        
+                    else
+                        log -v iptv "file $EPG_CACHE_PATH$file exists"
+                    fi
+
+                done
+
+                count=$(expr $count + 1)
             done
 
-            count=$(expr $count + 1)
         done
 
+    else
+        log -v iptv "EPG recently updated ($(var epgUpdatedDate)). Skipping."
+    fi
+
+    #
+    # Delete old files
+    #
+    files=$(find $EPG_CACHE_PATH -type f -mtime +$(var IPTV_DAYS) -print)
+    count=$(echo "$files" | sed '/^\s*$/d' | wc -l)
+    
+    log -d iptv "Deleting epg older than $(var IPTV_DAYS) days ($count files)"
+
+    for file in $files ; do
+        log -v iptv "Deleting file: $file"
+        rm -f $file
     done
 
     #
-    # Create main epg file
+    # Create main epg file if updated or file doesn't exist
     #
-    rm -f $CACHE_PATH"epg.xml"
+    if [ "$(var epgUpdated)" = "true" ] || [ ! -f $EPG_OUTPUT_PATH"epg.xml" ] ; then
 
-    for file in $(ls $EPG_CACHE_PATH) ; do
-        if [ -f $CACHE_PATH"epg.xml" ] ; then
+        log -d "Merging epg"
 
-            log -i iptv "Merging $file"
-            tv_merge -i $CACHE_PATH"epg.xml" -m $EPG_CACHE_PATH$file -o $CACHE_PATH"epg.xml"
+        rm -f $EPG_OUTPUT_PATH"epg-tmp.xml"
 
-        else
+        for file in $(ls $EPG_CACHE_PATH) ; do
+            if [ -f $EPG_OUTPUT_PATH"epg-tmp.xml" ] ; then
 
-            log -i iptv "Copying $file"
-            cp $EPG_CACHE_PATH$file $CACHE_PATH"epg.xml"
+                log -v iptv "Merging $file"
+                tv_merge -i $EPG_OUTPUT_PATH"epg-tmp.xml" -m $EPG_CACHE_PATH$file -o $EPG_OUTPUT_PATH"epg-tmp.xml"
 
-        fi
-    done
+            else
+
+                log -v iptv "Copying $file"
+                cp $EPG_CACHE_PATH$file $EPG_OUTPUT_PATH"epg-tmp.xml"
+
+            fi
+        done
+
+        log -v "Moving "$EPG_OUTPUT_PATH"epg-tmp.xml to "$EPG_OUTPUT_PATH"epg.xml"
+        mv -f $EPG_OUTPUT_PATH"epg-tmp.xml" $EPG_OUTPUT_PATH"epg.xml"
+
+    else
+        log -d "No updates. Skipping merge."
+    fi
+
+    var epgUpdated false
 
     sleep 600;
 done
