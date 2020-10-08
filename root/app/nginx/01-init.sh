e056aab675bff6c1db1565ebcccea741e3932e09
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-10-08 21:59:13 +0200

    Update 01-init.sh

--- root/app/nginx/01-init.sh
+++ root/app/nginx/01-init.sh
@@ -17,5 +17,5 @@ function add
 add svt1.svt.se 11001 "SVT 1" "https://svt1-a.akamaized.net/se/svt1/master.m3u8?defaultSubLang=1"
 add svt2.svt.se 11002 "SVT 2" "https://svt2-a.akamaized.net/se/svt2/master.m3u8?defaultSubLang=1"
 
-add nrk1.nrk.no 11001 "NRK 1" "https://nrk-nrk1.akamaized.net/21/1/hls/nrk_1/playlist.m3u8?bw_low=10&bw_high=3500&bw_start=1200&no_iframes&no_audio_only"
+add nrk1.nrk.no 11101 "NRK 1" "https://nrk-nrk1.akamaized.net/21/1/hls/nrk_1/playlist.m3u8?bw_low=10&bw_high=3500&bw_start=1200&no_iframes&no_audio_only"
 add nrk2.nrk.no 11102 "NRK 2" "https://nrk-nrk2.akamaized.net/22/1/hls/nrk_2/playlist.m3u8?bw_low=10&bw_high=3500&bw_start=1200&no_iframes&no_audio_only"
